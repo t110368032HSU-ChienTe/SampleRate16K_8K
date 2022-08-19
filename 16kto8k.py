8165ad5dc355597e0197ec5eb1e6f0af5c030fcb
t110368032HSU-ChienTe <93695115+t110368032HSU-ChienTe@users.noreply.github.com>
2022-08-19 16:16:05 +0800

    Add files via upload

--- 16kto8k.py
+++ 16kto8k.py
@@ -1,4 +1,5 @@
-from pydub import AudioSegment
+from sys import orig_argv
+import librosa
 import glob
 import os
 data_folder ='clean'
@@ -12,9 +13,12 @@ def main(wavs_list=wavs_list):
         os.mkdir(save_folder)
     for wav in wavs_list:
         print(f'處理{wav}')
-        wav_filename=f'{save_folder}/{wav}'
-        song=AudioSegment.from_wav(f'{data_folder}/{wav}')
-        song.set_frame_rate(sample_rate).export(wav_filename,format="wav")
+        wav_filename= wav.replace(data_folder,save_folder)
+        song_16K,sr=librosa.load(wav,16000)
+        song_8K=librosa.resample(y=song_16K,orig_sr=16000,targe=8000)
+        librosa.output.write_wav(wav_filename,song_8K,8000)
+        
+
 
 
 main()
