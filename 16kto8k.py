c4291cd4fbb431f6eb6d5e4b4ea9b3135715c65a
t110368032HSU-ChienTe <93695115+t110368032HSU-ChienTe@users.noreply.github.com>
2022-08-19 15:46:41 +0800

    Add files via upload

---
+++ 16kto8k.py
@@ -0,0 +1,23 @@
+import imp
+from pydub import AudioSegment
+import glob
+import os
+data_folder ='clean'
+save_folder ='clean_8K'
+sample_rate=8000
+
+wavs_list= glob.glob(f'{data_folder}/*.wav')
+
+def main(wavs_list=wavs_list):
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    for wav in wavs_list:
+        print(f'處理{wav}')
+        wav_filename=f'{save_folder}/{wav}'
+        song=AudioSegment.from_wav(f'{data_folder}/{wav}')
+        song.set_frame_rate(sample_rate).export(wav_filename,format="wav")
+
+
+main()
+
+
